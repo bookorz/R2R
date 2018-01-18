a8ba309a76cc53271cd5435d1cb6fb401f7cf601
weichien <souwaikin@gmail.com>
2018-01-18 08:48:11 +0800

    20180118 add cf coater

--- src/com/innolux/R2R/R2R_Main.java
+++ src/com/innolux/R2R/R2R_Main.java
@@ -20,6 +20,7 @@ public class R2R_Main {
 		//new RvDispatch().StartListen("tcp:8585", "INNOLUX.T2.PROD.PDS.PDSGLASSSEND.CELL.>", "8585", "");
 		new RvDispatch().StartListen("tcp:8585", "INNOLUX.T2.PROD.PDS.PDSGLASSSEND.CF.>", "8585", "");
 		new ArrayExp("Y:\\array\\cdol\\r2r\\source\\", "Y:\\array\\cdol\\r2r\\ng\\");
+		new CFCoater("Y:\\cf\\psheight\\r2r\\source\\", "Y:\\cf\\psheight\\r2r\\ng\\");
 		new RvDispatch().StartListen("tcp:8585", "INNOLUX.T2.PROD.PDS.PDSGLASSSEND.CELL.>", "8585", "");
 		//new RvDispatch().StartListen("tcp:8585", "INNOLUX.T2.PROD.PDS.PDSGLASSSEND.CF.>", "8585", "");
 		//new ArrayExp("C:\\AryExp\\");
